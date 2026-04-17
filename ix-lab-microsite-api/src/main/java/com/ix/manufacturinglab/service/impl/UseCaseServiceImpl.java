89c6d07f001dfe6f3786fd5f5e793d7eab90ea43
Sowjanya <ext.Dali.Sowjanya.Alla@skoda-auto.cz>
2026-04-17 16:33:30 +0530

    Commit contains fix for speakers UI exception handling, update artifacts, upload file size 200MB

--- ix-lab-microsite-api/src/main/java/com/ix/manufacturinglab/service/impl/UseCaseServiceImpl.java
+++ ix-lab-microsite-api/src/main/java/com/ix/manufacturinglab/service/impl/UseCaseServiceImpl.java
@@ -1,7 +1,12 @@
 package com.ix.manufacturinglab.service.impl;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
