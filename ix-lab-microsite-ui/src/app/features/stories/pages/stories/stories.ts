3930bd49060c5b282685a2f93056c2f01198faf6
Kumar Maiti <mainak.kumar.maiti@accenture.com>
2026-05-22 15:51:55 +0530

    bug fix

--- ix-lab-microsite-ui/src/app/features/stories/pages/stories/stories.ts
+++ ix-lab-microsite-ui/src/app/features/stories/pages/stories/stories.ts
@@ -475,15 +475,15 @@ export class StoriesComponent implements OnInit {
           this.applyFilters();
           this.cdr.detectChanges();
         }
-        // if (selectedIndustryObj) {
-        //   this.selectedIndustryId = selectedIndustryObj.industryId;
-        //   this.industryName = selectedIndustryObj.industryName;
+        if (selectedIndustryObj) {
+          this.selectedIndustryId = selectedIndustryObj.industryId;
+          this.industryName = selectedIndustryObj.industryName;
 
-        //   // 🔥 Trigger the same cascade as if user clicked the dropdown
-        //   this.onIndustrySelected(selectedIndustryObj);
+          // 🔥 Trigger the same cascade as if user clicked the dropdown
+          this.onIndustrySelected(selectedIndustryObj);
 
-        //   this.cdr.detectChanges();
-        // }
+          this.cdr.detectChanges();
+        }
 
         this.loadAllStories(this.currentPage, this.pageSize);
       } else {
